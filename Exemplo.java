0e44166460310e83c37d610d69d90f764daff0ed
Lennon <lennonpacola000@gmail.com>
2023-08-22 20:57:18 -0300

    Aula Finalizada

---
+++ Exemplo.java
@@ -0,0 +1,8 @@
+public class Exemplo {
+    public static void main(String[] args) {
+        
+        Aluno al = new Aluno();
+        al.setNome("Lennon");
+        System.out.println(al.getNome());
+    }
+}
